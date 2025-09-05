329852580697d44354413d019c31f93f4500c752
Notferu18 <hydrovfx1@gmail.com>
2025-09-06 01:06:44 +0800

    Add registerUser function to handle user registration

--- invent/src/AuthContext.jsx
+++ invent/src/AuthContext.jsx
@@ -6,7 +6,19 @@ const [currentUser, setCurrentUser] = useState(null);
 const [isRegistering, setIsRegistering] = useState(false);
 const [users, setUsers] = useState([{ username: "admin", password: "1234" }]);
 
+function registerUser(newUser) {
+    const usernameExists = users.some((user) => user.username === newUser.username);
+    if (usernameExists) {
+      alert("Username already exists!");
+      return;
+    }
+    setUsers([...users, newUser]);
+    setCurrentUser(newUser);
+    setIsRegistering(false);
+  }
 
+
+  
 
 
 
